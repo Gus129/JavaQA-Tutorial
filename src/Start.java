5b233a4b188c2b9392b04f571c4192ac11c3b635
GUS\Vladimir <atis-magnus@mail.ru>
2022-12-01 16:07:15 +0200

    Data types with examples and memory usage.

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,16 @@
 public class Start {
     public static void main(String[] args) {
         System.out.println("Hello Plebs!!!");
+        byte num = 100; // 8 бит информации на эту переменную нум  -128 +127
+        short num1 = 131; // 16 бит -32768 +32767
+        int num2 = 141; // 32 бит -2147483648 +2147483647
+        long num3 = 123; // 64 бит -9223372036854775808 до 9223372036854775807
+
+        float num4 = 1.1f; // 32 бит 1.123456 6-7 знаков после запятой
+        double num5 = 1.11d; // 64 бит (d можно не писать) 15-знаков точность после запятой
+
+        char sym = '\u2122'; // 16 бит, значок "тм"
+
+        boolean isTrue = false; // 1 бит true;
     }
 }
